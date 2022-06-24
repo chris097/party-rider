635798a83ce64fa4969bd99b196dd74cacd9701a
chris097 <christian@esettlementgroup.com>
2022-06-24 02:04:16 +0100

    initial commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -25,16 +25,16 @@ const Home = () => {
                   </div>
                 <div>
                     <h1 className="text-2xl md:text-5xl">Welcome To</h1>
-                    <h1 className="text-5xl sm:text-5xl lg:text-7xl my-3">Party Rider Club</h1>
-                    <h3 className="text-xl">Find private parties happening near you fast</h3>
+                    <h1 className="text-3xl sm:text-5xl lg:text-7xl my-3">Party Rider Club</h1>
+                    <h3 className="md:text-xl text-xs">Find private parties happening near you fast</h3>
                 </div>
-                  <div className='bg-primaryColor w-full md:w-2/5 my-6 rounded-full h-14 m-auto flex items-center justify-center text-2xl'>
-                      <span className='text-3xl mr-2'>340,200</span>
+                  <div className='bg-primaryColor w-52 md:w-2/5 px-2 my-6 rounded-full h-14 m-auto flex items-center justify-center text-xl md:text-2xl'>
+                      <span className='text-xl md:text-3xl mr-2'>340,200</span>
                       Members
                   </div>
-                  <div className="border rounded-full h-16 flex mt-8">
+                  <div className="border rounded-full h-10 md:h-16 flex mt-8">
                       <input className="w-full bg-transparent px-5 outline-none focus:border-white placeholder:text-white" type="text" placeholder='Enter email address' />
-                      <button className="w-40 border-l text-white rounded-tr-full rounded-br-full hover:bg-slate-400 bg-red-400 px-4">Join WaitList</button>
+                      <button className="w-40 border-l text-white rounded-tr-full rounded-br-full md:text-lg text-sm hover:bg-slate-400 bg-red-400 px-4 whitespace-nowrap">Join WaitList</button>
                   </div>
                   <div className='mt-12 mb-6'>Visit Our Social Media</div>
                   <div className='flex justify-center space-x-10'>
